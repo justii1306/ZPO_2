5feb19a17c9df1c94f86a9ab2db40b227417923a
justii1306 <35031460+justii1306@users.noreply.github.com>
2018-10-16 07:19:39 +0200

    Add files via upload

---
+++ ZPO_2/src/TWO.java
@@ -0,0 +1,5 @@
+public class TWO {
+    public void m1(){
+        System.out.println("Metoda z klasy TWO");
+    }
+}
---
+++ ZPO_2a/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        TWO test = new TWO();
+        test.m1();
+    }
+}
